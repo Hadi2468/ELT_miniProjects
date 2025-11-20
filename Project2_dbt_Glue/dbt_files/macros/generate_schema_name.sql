e7768d721dff2f4d72b7ee7cf619a4d1674a9a2a
Hadi Hosseini <dr.hadihosseini@gmail.com>
2025-11-19 22:18:07 -0800

    Add files via upload

---
+++ Project2_dbt_Glue/dbt_files/macros/generate_schema_name.sql
@@ -0,0 +1,14 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+ 
+    {%- set default_schema = target_schema -%}
+    {%- if custom_schema_name is none -%}
+ 
+        {{ default_schema }}
+ 
+    {%- else -%}
+ 
+        {{ custom_schema_name | trim }}
+ 
+    {%- endif -%}
+ 
+{%- endmacro %}
